62c15edac3a1f7cefb9428aba7b2c966d85f9ece
David Barber <barberdr@gmail.com>
2016-09-19 21:07:02 -0500

    Initial commit

---
+++ Assignment01/Assignment01/InstrumentationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InstrumentationViewController.swift
+//  Assignment01
+//
+//  Created by David Barber on 9/18/16.
+//  Copyright © 2016 David Barber. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
